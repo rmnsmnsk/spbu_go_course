232d7e9a5a83b48bf567ce67e6df4d52e4a5fb09
Roman Semensky <roltiserplot@gmail.com>
2026-09-21 20:30:09 +0300

    Added input

--- hw_2/main.go
+++ hw_2/main.go
@@ -4,12 +4,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"flag"
+	"time"
 )
 
 func main(){
 
-	resp, err := http.Get("https://homeworksite.site/1/info.0.json");
+	from := flag.Int("from", 0, "id первого фильма")
+	to := flag.Int("to", 0, "id последнего фильма")
+	workers := flag.Int("workers", 0, "сколько воркеров")
+	timeout := flag.Duration("timeout", 5 * time.Second, "сколько таймаут HTTP запроса")
 
+	flag.Parse()
+
+	fmt.Println("from:", *from)
+	fmt.Println("to:", *to)
+	fmt.Println("workers:", *workers)
+	fmt.Println("timeout:", *timeout)
+
+	resp, err := http.Get("https://homeworksite.site/1/info.0.json");
 
 	if err != nil{
 		fmt.Println("request error:", err)
